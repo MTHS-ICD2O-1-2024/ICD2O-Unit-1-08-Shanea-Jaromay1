667ad873b399ebee3f8c139e00e7c45386e1ff57
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-04 18:14:57 +0000

    changed code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Copyright (c) 2025 Shanea Jaromay All rights reserved
+// Copyright (c) 2025 Shanea Jaromay. All rights reserved.
 //
 // Created by: Shanea Jaromay
-// Created on: Feb 2025
-// This file contains the JS functions for index.html
+// Created on: February 2025
+// This file contains the JavaScript functions for index.html
 
 function myButtonClicked() {
-  alert('Hello, World!')
+  alert('Hello, World!');
 }
