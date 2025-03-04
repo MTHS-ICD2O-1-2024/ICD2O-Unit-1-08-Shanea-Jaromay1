33acb05bc3e7af072c9a552427046b448d6a0a37
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-04 03:29:05 +0000

    changed code

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <html lang="en-ca">
   <head>
     <meta charset="utf-8" />
-    <meta name="description" content="The XXX program" />
+    <meta name="description" content="The XXX program, PHP" />
     <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Shanea Jaromay" />
+    <meta name="author" content="Shaena Jaromay" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
       rel="stylesheet"
@@ -13,7 +13,7 @@
     />
     <link
       rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.blue-indigo.min.css"
+      href="https://code.getmdl.io/1.3.0/material.green-red.min.css" /> "
     />
     <link rel="stylesheet" href="./css/style.css" />
     <link
@@ -34,25 +34,25 @@
       href="./images/favicon-16x16.png"
     />
     <link rel="manifest" href="./images/site.webmanifest" />
-    <title>The XXX program, JS</title>
+    <title>The XXX program, PHP</title>
   </head>
+
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <script src="./js/script.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <header class="mdl-layout__header">
         <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The XXX program, JS</span>
+          <span class="mdl-layout-title">The XXX program, PHP</span>
         </div>
       </header>
       <main class="mdl-layout__content">
         <div class="right-image">
-          <img src="./images/flower.jpeg" alt="flower />
+          <img src="./images/flower.jpeg" alt="flower" />
         </div>
         <div class="page-content">Instructions here ...</div>
         <br />
         <!-- Simple Textfield for integers-->
-        <form action="#">
+        <form action="answer.php" method="GET">
           <div class="mdl-textfield mdl-js-textfield">
             <!-- input pattern attribute -->
             <input
@@ -72,16 +72,12 @@
           <!-- Accent-colored raised button with ripple -->
           <button
             class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            onclick="myButtonClicked()"
-            type="button"
+            type="submit"
           >
             Calculate
           </button>
         </form>
         <br />
-        <div class="page-content-answer">
-          <div id="answer">The answer is: ...</div>
-        </div>
       </main>
     </div>
   </body>
